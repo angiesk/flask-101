f0fa082fdda4ab23621017ddc9fe402740353c4e
angiesk <shivangi2197@hotmail.com>
2019-02-08 17:43:03 +0530

    Finished Home component

---
+++ user_login_reg/client/src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="conatiner">
+    <div class="jumbotron mt-5">
+      <div class="col-sm-8 mx-auto">
+        <h1 class="text-center">Hello Everyone</h1>
+
+      </div>
+    </div>
+  </div>
+</template>
